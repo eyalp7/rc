aced955b38c3ade1ee49f2ead4f7fcdb8b0caefb
eyalp7 <eyalp717@gmail.com>
2024-11-19 23:38:40 +0200

    .

--- client.py
+++ client.py
@@ -9,119 +9,92 @@ import cv2
 import struct
 import keyboard
 
-# Server configuration settings
+#Server configuration settings
 IP = "10.0.0.7"
-CLICKS_PORT = 12345
+EVENTS_PORT = 12345
 SCREENSHOTS_PORT = 54321
-MOUSE_PORT = 12346
 
-print(f"Connecting to the events socket in {IP}:{CLICKS_PORT}... ")
-clicks_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # The socket must be a global variable for use in different functions.
-clicks_socket.connect((IP, CLICKS_PORT))
-
-def authentication(client_socket):
-    """Logs into the server with the password."""
-    message = client_socket.recv(1024).decode('utf-8')
-    print(message)
-
-    while(message != "SUCCESS"):
-        # Loop until the server sends SUCCESS (meaning password was correct)
-        password = input("Enter password: ")
-        client_socket.send(password.encode('utf-8'))
-        message = client_socket.recv(1024).decode('utf-8')
-        print(message)
+print(f"Connecting to the events socket in {IP}:{EVENTS_PORT}... ")
+events_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #The socket has to be a global variable so it can be used in the different functions.
+events_socket.connect((IP, EVENTS_PORT))
 
 def new_key(event):
     """ Sends keyboard presses to the server."""
     button = event.name
     message = json.dumps({"action": "keyboard_press", "key": button}).encode()
-    clicks_socket.send(message)
+    events_socket.send(message)
+
+def on_move(x, y):
+    """ Sends mouse movement information to the server."""
+    message =json.dumps({"action": "mouse_movement", "x": x, "y": y}).encode() #Serialization
+    events_socket.send(message)
+    time.sleep(0.05)
 
 def on_click(x, y, button, pressed):
     """ Sends a mouse click to the server."""
     if pressed:
-        message = json.dumps({"action": "mouse_click", "x": x, "y": y, "button": str(button)}).encode()  # Serialization
-        clicks_socket.send(message)
+        message = json.dumps({"action": "mouse_click", "x": x, "y": y, "button": str(button)}).encode() #Serialization
+        events_socket.send(message)
+        time.sleep(0.05)
 
 def on_scroll(x, y, dx, dy):
     """ Sends a mouse scroll to the server."""
-    message = json.dumps({"action": "mouse_scroll", "x": x, "y": y, "dx": dx, "dy": dy}).encode()  # Serialization
-    clicks_socket.send(message)
+    message = json.dumps({"action": "mouse_scroll", "x": x, "y": y, "dx": dx, "dy": dy}).encode() #Serialization
+    events_socket.send(message)
+    time.sleep(0.05)
 
 def handle_keyboard():
-    """Listens for keyboard presses."""
+    """Listens for keyboard presses. """
     keyboard.on_release(callback=new_key)
     keyboard.wait()
 
 def handle_mouse():
-    """Tracks mouse events and handles them."""
+    """Tracks mouse events and handles them"""
     with pynput.mouse.Listener(on_click=on_click, on_scroll=on_scroll) as listener:
-        listener.join()  # Starts tracking mouse events.
+        listener.join() #Starts tracking mouse events.
 
 def handle_mouse_movements():
-    """Sends the current mouse location to the server."""
-    mouse_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    address = (IP, MOUSE_PORT)
+    """Sends 4 times every second information about the mouse movement"""
     while True:
         x, y = pyautogui.position()
-        message = json.dumps({"action": "mouse_movement", "x": x, "y": y}).encode()  # Serialization
-        mouse_socket.sendto(message, address)
-        time.sleep(0.05)
+        message =json.dumps({"action": "mouse_movement", "x": x, "y": y}).encode() #Serialization
+        events_socket.send(message)
+        time.sleep(0.25)
 
 def handle_screenshots(screenshots_socket):
-    """Receives screenshots from the server and displays them."""
+    """Receives screenshots from the server and displays them. """
     while True:
-        data_length = screenshots_socket.recv(4)  # The first 4 bytes indicate the image size.
+        data_length = screenshots_socket.recv(4) #The first 4 bytes indicate the image size.
         if not data_length:
             break
-        data_length = struct.unpack("L", data_length)[0]  # Unpacks the first 4 bytes into an integer.
-
+        data_length = struct.unpack("L", data_length)[0] #Unpacks the first 4 bytes into an integer.
         data = b''
         while len(data) < data_length:
-            # The loop receives packets until the entire image is received.
+            #The loop receives packets untill the entire image is received.
             packet = screenshots_socket.recv(data_length - len(data))
             if not packet:
                 break
             data += packet
         
-        numpy_img = np.frombuffer(data, dtype=np.uint8)  # Convert the bytes to a numpy array.
-        img = cv2.imdecode(numpy_img, cv2.IMREAD_COLOR)  # Convert the numpy array back to an image.
-
-        if img is not None:
-            cv2.imshow("Server's view: ", img)
-
-        if cv2.waitKey(1) == 2490368:  # The keycode for 'f8'.
+        
+        numpy_img = np.frombuffer(data, dtype=np.uint8) #Convert the bytes to a numpy array.
+        img = cv2.imdecode(numpy_img, cv2.IMREAD_COLOR) #Convert the numpy array back to an image.
+        cv2.imshow("Server's view: ", img)
+        if cv2.waitKey(1) == ord('q'):
             break
 
 def connect_to_screenshots_server():
-    """Connects to the screenshots socket."""
+    """Connects to the screenshots relating socket. """
     print(f"Connecting to the screenshots server in {IP}:{SCREENSHOTS_PORT}")
     screenshots_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     screenshots_socket.connect((IP, SCREENSHOTS_PORT))
-    print("Connected.")
-
-    handle_screenshots(screenshots_socket)
+    screenshots_thread = threading.Thread(target=handle_screenshots, args=(screenshots_socket,)).start()
 
 def main():
-    """The main function that calls all of the needed functions."""
-    authentication(clicks_socket)
-    
-    # Start threads for handling various tasks
-    screenshots_thread = threading.Thread(target=connect_to_screenshots_server)
-    mouse_thread = threading.Thread(target=handle_mouse)
-    keyboard_thread = threading.Thread(target=handle_keyboard)
-    movement_thread = threading.Thread(target=handle_mouse_movements)
-
-    screenshots_thread.start()
-    mouse_thread.start()
-    keyboard_thread.start()
-    movement_thread.start()
-
-    # Wait for all threads to finish
-    screenshots_thread.join()
-    mouse_thread.join()
-    keyboard_thread.join()
-    movement_thread.join()
-
+    """The main function that calls all of the needes function. """
+    threading.Thread(target=connect_to_screenshots_server).start()
+    mouse_thread = threading.Thread(target=handle_mouse).start()
+    keyboard_thread = threading.Thread(target=handle_keyboard).start()
+    movement_thread = threading.Thread(target=handle_mouse_movements).start()
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -9,144 +9,108 @@ import cv2
 import struct
 import threading
 
-# Server configuration settings.
-IP = "10.0.0.4"
-KEYBOARD_PORT = 12345
+#Server configuration settings.
+IP = "10.0.0.7" #okay nice this is working very nicely
+EVENTS_PORT = 12345
 SCREENSHOTS_PORT = 54321
-MOUSE_PORT = 12346
-PASSWORD = "password123"
+special_keys = ['ctrl', 'right ctrl', 'alt', 'right alt', 'shift', 'right shift'] #Special keys that are usually pressed with another key.
+last_key = "_" #The last key that the client pressed.
 
-special_keys = ['ctrl', 'right ctrl', 'alt', 'right alt', 'shift', 'right shift']  # Special keys that are usually pressed with another key.
-last_key = "_"  # The last key that the client pressed.
-
-def authentication(client_socket):
-    """Gets the password from the client."""
-    client_socket.send("Enter the password: ".encode('utf-8'))
-    while True:
-        given_password = client_socket.recv(1024).decode('utf-8')
-        if(given_password == PASSWORD):
-            client_socket.send("SUCCESS".encode('utf-8'))
-            break
-        client_socket.send("Invalid password. ".encode('utf-8'))
 
 def mouse_click(event):
-    """Clicks the mouse at the client's mouse location."""
-    if event['button'] == 'Button.left':
+    """Clicks the mouse at the client's mouse location. """
+    pyautogui.moveTo(event['x'], event['y'], 0.1)
+    if(event['button'] == 'Button.left'):
         mouse.click('left')
-    elif event['button'] == 'Button.right':
+    elif(event['button'] == 'Button.right'):
         mouse.click('right')
-    elif event['button'] == 'Button.middle':
-        mouse.click('middle')
+    elif(event['button'] == 'Button.middle'):
+        mouse_click('middle')
 
 def mouse_movement(event):
-    """Moves the mouse to the client's mouse location."""
-    pyautogui.moveTo(event['x'], event['y'], 0.01)
+    """Moves the mouse to the client's mouse location. """
+    pyautogui.moveTo(event['x'], event['y'], 0.1)
 
 def mouse_scroll(event):
     """Scrolls the mouse."""
     pyautogui.scroll(event['dy'])
 
 def keyboard_press(event):
-    """Presses the key that the client pressed."""
+    """Presses the key that the client pressed. """
     global last_key
     key = event["key"]
-    if last_key in special_keys:  # In case that the last character is a special character.
-        key = last_key + "+" + key  # Presses both of the keys at the same time.
+    if(last_key in special_keys): #In case that the last character is a special character.
+        key = last_key + "+" + key #Presses both of the keys at the same time.
     keyboard.press_and_release(key)
     last_key = key
 
-def handle_presses(client_socket):
-    """Handles presses received from the client."""
+def handle_events(client_socket):
+    """Handles events received from the client."""
     while True:
         message = client_socket.recv(1024).decode('utf-8')
-        event = json.loads(message)  # Deserialization
+        event = json.loads(message) #Deserialization
 
         action = event['action']
-        if action == "mouse_click":
+        if(action == "mouse_click"):
             mouse_click(event)
-        elif action == "keyboard_press":
+        elif(action == "keyboard_press"): 
             keyboard_press(event)
-        elif action == "mouse_scroll":
+        elif(action == "mouse_movement"):
+            mouse_movement(event)
+        elif(action == "mouse_scroll"):
             mouse_scroll(event)
+        print(event)
 
 def handle_screenshots(client_socket):
-    """Takes screenshots and sends them to the client."""
+    """Takes screenshots and sends them to the client. """
     w, h = pyautogui.size()
 
     with mss.mss() as sct:
         monitor = {"top": 0, "left": 0, "width": w, "height": h}
+        #An infinite loop that continually captures screenshots and sends them to the client.
         while True:
             img = sct.grab(monitor)
-            numpy_img = np.array(img)  # Converting to a numpy array for better processing.
+            numpy_img = np.array(img) #Converting to a numpy array for better processing.
 
             cursor_x, cursor_y = pyautogui.position()
 
-            # Draw a white dot at the cursor's position
             radius = 5
-            color = (255, 255, 255)  # White color in BGR
-            thickness = -1  # Solid circle
+            color = (255, 255, 255)
+            thickness = -1
             numpy_img = cv2.circle(numpy_img, (cursor_x, cursor_y), radius, color, thickness)
 
             _, encoded_img = cv2.imencode('.jpg', numpy_img)
-            data = encoded_img.tobytes()  # Converting the image to bytes.
+            data = encoded_img.tobytes() #Converting the image to bytes.
 
-            client_socket.sendall(struct.pack("L", len(data)) + data)  # Sends the length of the screenshot first as an unsigned long.
+            client_socket.sendall(struct.pack("L", len(data)) + data) #Sends the length of the screenshot first as an unsigned long.
 
-            if keyboard.is_pressed('f8'):
+            if cv2.waitKey(1) == ord('q'):
                 break
 
-def handle_mouse_movements(mouse_socket):
-    """Handles the mouse movements of the client."""
-    while True:
-        message, _ = mouse_socket.recvfrom(1024)
-        mouse_movement(json.loads(message.decode('utf-8')))
+def start_events_server():
+    """Starts the server that handles keyboard and mouse events. """
+    print(f"Starting events server on: {IP}:{EVENTS_PORT}...")
+    event_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    event_socket.bind((IP, EVENTS_PORT))
 
-def start_main_server():
-    """Starts the server that handles keyboard and mouse events + authentication."""
-    print(f"Starting events server on: {IP}:{KEYBOARD_PORT}...")
-    main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    main_socket.bind((IP, KEYBOARD_PORT))
-
-    main_socket.listen(1)
-    client_socket, address = main_socket.accept()
+    event_socket.listen(1)
+    client_socket, address = event_socket.accept()
     print(f"Connection established from: {address}")
 
-    authentication(client_socket)
-
-    clicks_thread = threading.Thread(target=handle_presses, args=(client_socket,))
-    screenshots_thread = threading.Thread(target=start_screenshots_server)
-    movements_thread = threading.Thread(target=start_mouse_server)
-
-    clicks_thread.start()
-    screenshots_thread.start()
-    movements_thread.start()
-
-    clicks_thread.join()
-    screenshots_thread.join()
-    movements_thread.join()
+    handle_events(client_socket)
 
 def start_screenshots_server():
-    """Starts the socket that handles the screenshots taking and sending."""
-    print(f"Starting screenshots server on: {IP}:{SCREENSHOTS_PORT}...")
+    """Starts the server that handles the screenshots taking and sending. """
+    print(f"Starting screenshots server on: {IP}:{SCREENSHOTS_PORT}")
     screenshots_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     screenshots_server.bind((IP, SCREENSHOTS_PORT))
-    
+
     screenshots_server.listen(1)
     client_socket, address = screenshots_server.accept()
     print(f"Connection established from: {address}")
 
     handle_screenshots(client_socket)
 
-def start_mouse_server():
-    """Starts the UDP socket that listens for mouse movements."""
-    mouse_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    mouse_socket.bind((IP, MOUSE_PORT))
-    print(f"Starting movements server on: {IP}:{MOUSE_PORT}...")
-
-    handle_mouse_movements(mouse_socket)
-
-def main():
-    start_main_server()
-
 if __name__ == '__main__':
-    main()
+    events_thread = threading.Thread(target=start_events_server).start()
+    screenshots_thread = threading.Thread(target=start_screenshots_server).start()
